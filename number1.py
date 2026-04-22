8f896ba2271c8170b4a8f4fac046c9123c4095a1
Muhammad Nizar Wirapradana <mnizarw1407@gmail.com>
2026-04-22 01:00:37 -0700

    Update number1.py

--- number1.py
+++ number1.py
@@ -5,14 +5,14 @@ char_count = 0
 in_word = False
 
 for char in text:
-    if char != '':
+    if char != " ":
         char_count += 1
         if not in_word:
             word_count += 1
             in_word = True
     else:
         in_word = False
-            
+
 print(f"Teks: {text}")
-print(f"Words= {word_count}")
-print(f"Characters = {char_count}")
+print(f"words = {word_count}")
+print(f"characters = {char_count}")
